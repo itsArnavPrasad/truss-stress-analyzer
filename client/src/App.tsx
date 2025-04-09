46a663d916842816c33d943d27c232b5bc81e15a
Arnav Prasad <thearnavprasad@gmail.com>
2025-04-09 20:10:03 +0530

    Removed unused components & removed 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,16 @@
 import { Switch, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
-import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      {/* Fallback to 404 */}
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
-function App() {
-  return (
     <>
-      <Router />
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
       <Toaster />
     </>
   );
 }
 
-export default App;
+export default Router;
